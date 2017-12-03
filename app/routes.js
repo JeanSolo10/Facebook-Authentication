e6f9070d09e339ac96e9f77ebbad2464e75e8a1f
JeanSolo10 <jeanpksb10@gmail.com>
2017-12-03 17:37:07 -0500

    Updated Version of FB Authentication
    + better results for authentication

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,6 @@ module.exports = function(app, passport){
     });
 
     app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['email'] }));
-//	app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['email', 'birthday'] }));
 
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', { successRedirect: '/profile',
